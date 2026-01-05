2a69e9182648c631663c1d73e56375b6249e3428
qsoul <137517686+c1osed1@users.noreply.github.com>
2026-01-05 18:24:32 +0300

    Initial Commit

---
+++ konnectapp/konnectappApp.swift
@@ -0,0 +1,17 @@
+//
+//  konnectappApp.swift
+//  konnectapp
+//
+//  Created by qsoul on 05.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct konnectappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
